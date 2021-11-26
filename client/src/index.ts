63df8af67783b09d21a4fdf68b8ea771a73d1b05
SU\pavlo.malashnyak <pavlo.malashnyak@sigma.software>
2021-11-26 18:17:55 +0200

    INT-8: Refactoring

--- client/src/index.ts
+++ client/src/index.ts
@@ -3,71 +3,76 @@ import type { Note } from './note.model';
 class Main {
     public static main(): void {
         class NotesGridCreator {
-            constructor(private readonly gridSelector: string, private readonly serverUrl: string) {}
+            constructor(
+                private readonly headerSelector: string, 
+                private readonly gridSelector: string,
+                private readonly serverUrl: string ) {}
 
             private fetchNotes(): Promise<Note[]> {
                 return fetch(this.serverUrl).then(res => res.json());
             }
 
-            private createNoteElement (note: Note) : HTMLTableRowElement {
-                const { id, title, author, description, date } = note;
+            private createHeaderElement (note: Note): HTMLTableRowElement {
+                const headerRow = document.createElement('tr');
 
+                Object.keys(note).forEach(key => {
+                    const tdHeader = document.createElement('td');
+                    tdHeader.setAttribute('class', 'grid-header'); 
+                    tdHeader.textContent = key.charAt(0).toUpperCase() + key.slice(1);
+                    headerRow.appendChild(tdHeader);
+                });
+                
+                return headerRow;
+            }
+            private createNoteElement (note: Note): HTMLTableRowElement {
+                const headerRow = document.createElement('tr');
                 const row = document.createElement('tr');
                 row.setAttribute('class', 'grid-row');
 
-                const idTd = document.createElement('td');
-                const titleTd = document.createElement('td');
-                const descriptionTd = document.createElement('td');
-                const authorTd = document.createElement('td');
-                const dateTd = document.createElement('td');
+                for (const [key, value] of Object.entries(note)) {
+                    const td = document.createElement('td');
+                    td.setAttribute('class', 'grid-data');
+                    
+                    if (key === 'description' || key === 'date') {
+                        const tooltip = document.createElement('my-tooltip');
+                        
+                        const valueToAppend = key === 'date' ? value.split(' ')[0] : value;
 
-                idTd.setAttribute('class', 'grid-data');
-                titleTd.setAttribute('class', 'grid-data');
-                descriptionTd.setAttribute('class', 'grid-data');
-                authorTd.setAttribute('class', 'grid-data');
-                dateTd.setAttribute('class', 'grid-data');
+                        tooltip.setAttribute('tooltip-text', value);
+                        tooltip.setAttribute('visible-text', valueToAppend);
             
-                idTd.textContent = id;
-                titleTd.textContent = title;
-                authorTd.textContent = author;
-            
-                const descriptionTooltip = document.createElement('my-tooltip');
-                descriptionTooltip.setAttribute('tooltip-text', description);
-                descriptionTooltip.setAttribute('visible-text', description);
-            
-                descriptionTd.appendChild(descriptionTooltip);
-            
-                const dateTooltip = document.createElement('my-tooltip');
-                dateTooltip.setAttribute('tooltip-text', date);
-                dateTooltip.setAttribute('visible-text', date.split(' ')[0]);
-            
-                dateTd.appendChild(dateTooltip);
-            
-                row.appendChild(idTd);
-                row.appendChild(titleTd);
-                row.appendChild(descriptionTd);
-                row.appendChild(authorTd);
-                row.appendChild(dateTd);
-            
+                        td.appendChild(tooltip);
+                        row.appendChild(td);
+                        continue;
+                    }
+
+                    td.textContent = value;
+                    row.appendChild(td);
+                  }
+                 
                 return row;
             }
 
             async renderTable() {
+                const noteHeader: HTMLElement|null = document.querySelector(this.headerSelector);
+
                 const notesGrid: HTMLElement|null = document.querySelector(this.gridSelector);
 
-                if (!notesGrid) {
+                if (!notesGrid || !noteHeader) {
                     throw new Error('INT: There is no such element on a page.')
                 }
 
                 const gridData = await this.fetchNotes();
 
+                noteHeader.appendChild(this.createHeaderElement(gridData[0]));
+                
                 gridData.forEach(note => {
                     notesGrid.appendChild(this.createNoteElement(note));
                 });
             }
         }
 
-        new NotesGridCreator('tbody', 'http://localhost:5000/').renderTable();
+        new NotesGridCreator('thead', 'tbody', 'http://localhost:5000/').renderTable();
     }
 }
 
